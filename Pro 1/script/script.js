075e2a1c6f4bfc86270f1cd903fa185d942f7938
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-10-10 18:47:39 -0300

    Adicionando nova tela inícial e carousel na área de atuação

--- Pro 1/script/script.js
+++ Pro 1/script/script.js
@@ -1,4 +1,32 @@
+
+/* -- Navbar -- */
 document.getElementById('menu-btn').addEventListener('click', function(){
     const nav = document.querySelector('nav');
     nav.classList.toggle('active');
+})
+
+/* -- Container activity area carousel -- */
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause:true,
+        smartSpeed: 1500,
+        responsive:{
+            0:{
+                item:1
+            },
+            600:{
+                item:1
+            },
+            1000:{
+                item:3
+            }
+        }
+    })
 })
